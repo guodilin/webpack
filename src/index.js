774a7ec2754d689ed8bb008262163a7dee25240a
guodilin <admin@guodilin.com>
2022-10-28 15:18:27 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // 使用es6导入语法，导入jQuery
 import $ from 'jquery'
 // 导入样式(在webpack中，一切皆模块，都可以通过es6导入语法进行导入和使用)
-import './css/style.css'
-import './css/style.less'
+import '@/css/style.css'
+import '@/css/style.less'
+import lofo from './file.png';
 
+console.log(lofo)
+$('.box').attr('src', lofo)
 
 $(function () {
     $('li:odd').css('background-color', 'red')
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path')
 // 1. 导入html-webpack-plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 // 使用 Node.js 中的导入语法，向外导出一个webpack的配置对象
 module.exports = {
     // mode 用来指定构建模式，可选值有 development 和 production
     // development 开发模式，production 线上模式
     mode: 'development',
+    // 报错行对准源代码
+    // devtool: 'eval-source-map',
+    // 定位行号，不暴露源码
+    devtool: 'nosources-source-map',
     // __dirname 当前文件目录
     entry: path.join(__dirname, './src/index.js'),  // 打包入口文件的路径
     // 指定生成的文件要存放到哪里
@@ -13,7 +20,7 @@ module.exports = {
         // 存放目录
         path: path.join(__dirname, 'dist'),   // 打包入口文件的路径
         // 生成的文件名
-        filename: 'bundle.js'
+        filename: 'js/bundle.js'
     },
     module: {
         rules: [
@@ -32,7 +39,21 @@ module.exports = {
                 test: /\.s[ac]ss$/i,
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
+            {
+                // 将文件转换为base64 URI的webpack加载程序。
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader?limit=80&outputPath=images',
+                    },
+                ],
+            },
         ],
+    },
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, './src/')
+        }
     },
     devServer: {
         open: true, // 初次打包完成后，自动打开浏览器
@@ -48,6 +69,8 @@ module.exports = {
             template: './src/index.html',
             // 指定复制出来的文件名和存放路径
             filename: './index.html'
-        })
+        }),
+        // 自动删除 dist
+        new CleanWebpackPlugin()
     ]
 }
